f13be68cab5762918485cefc9987f50d2546df32
tore gard <dragerot@gmail.com>
2023-01-17 00:29:48 +0100

    removed qr reader

--- pages/scan.js
+++ pages/scan.js
@@ -1,5 +1,5 @@
 import React, { useState, useRef } from "react";
-import { QrReader } from "react-qr-reader";
+
 import styles from "../styles/Home.module.css";
 
 function Scan() {
@@ -8,23 +8,7 @@ function Scan() {
   return (
     <div className={styles.container}>
       <div className={styles.container}>
-        <QrReader
-          onResult={(result, error) => {
-            if (!!result) {
-              setData(result?.text);
-            }
-            if (!!error) {
-              console.info("***********");
-              console.info(error);
-            }
 
-          }
-        }
-//this is facing mode : "environment " it will open backcamera of the smartphone and if not found will
-// open the front camera
-        constraints    ={{ facingMode:  "environment"  }}
-          style={{ width: "40%", height: "40%" }}
-        />
         <p>{data}</p>
 
       </div>
